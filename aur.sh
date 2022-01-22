0449e97d80583d9c4c5c74c8279bbb77efa98db5
Adam Sherif <38723412+MrKioZ@users.noreply.github.com>
2022-01-22 14:59:04 +0200

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,9 +1,58 @@
 #!/bin/bash
-d=${BUILDDIR:-$PWD}
-for p in ${@##-*}
-do
-cd "$d"
-curl "https://aur.archlinux.org/packages/${p:0:2}/$p/$p.tar.gz" |tar xz
-cd "$p"
-makepkg ${@##[^\-]*}
-done
+if [ -z "$1" ]
+then
+    echo "=================================================="
+    echo "specify a package to install duhh"
+    echo "=================================================="
+    exit 1
+else
+    if ! git clone "https://aur.archlinux.org/$1.git"
+    then
+        echo "=================================================="
+        echo "FATAL: No such pacakge found on aur"
+        echo "=================================================="
+        exit 1
+    else
+        echo "=================================================="
+        echo "Starting package build"
+        echo "=================================================="
+        cd $1
+        if ! makepkg -si
+        then
+            echo "=================================================="
+            echo "FATAL: Could not build the package, removing directory..."
+            echo "=================================================="
+            cd ..
+            if ! rm -rf $1
+            then
+                echo "=================================================="
+                echo "ERROR: Could not delete the source directory, you can delete it yourself tho"
+                echo "=================================================="
+                exit 1
+            else
+                echo "=================================================="
+                echo "successfully removed dir $1"
+                echo "=================================================="
+                exit 0
+            fi
+            exit 1
+        else
+            echo "=================================================="
+            echo "Sucessfully built package! Removing src directory"
+            echo "=================================================="
+            cd ..
+            if ! rm -rf $1
+            then
+                echo "=================================================="
+                echo "ERROR: Could not delete the source directory, you can delete it yourself tho"
+                echo "=================================================="
+                exit 1
+            else
+                echo "=================================================="
+                echo "successfully installed $1!!"
+                echo "=================================================="
+                exit 0
+            fi
+        fi
+    fi
+fi
