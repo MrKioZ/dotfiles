0df83fbade0d2c5304753f65c25293aec25f0894
Adam Sherif <38723412+MrKioZ@users.noreply.github.com>
2022-01-22 16:45:07 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,11 +4,13 @@ sudo pacman -Syyu
 sudo pacman -Sy --noconfirm --needed git go
 
 if ! yay
+then
     echo "[x] Please make sure you have yay installed"
     echo "[*] Use the following commands:"
     echo "[*] git clone https://aur.archlinux.org/yay.git"
     echo "[*] cd yay"
     echo "[*] makepkg -si"
     exit
-   
+fi
+
 yay -S rofi lightdm vscodium-bin alacritty ranger
