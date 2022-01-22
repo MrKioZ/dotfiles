b3377d946d0ae7118493814a22e7ec34803ad711
Adam Sherif <38723412+MrKioZ@users.noreply.github.com>
2022-01-22 14:00:37 +0200

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -eu
+
+# Arch Linux Install Script (alis) installs unattended, automated
+# and customized Arch Linux system.
+# Copyright (C) 2022 picodotdev
+
+GITHUB_USER="MrKioZ"
+BRANCH="master"
+
+while getopts "u:" arg; do
+  case ${arg} in
+    u)
+      GITHUB_USER=${OPTARG}
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+rm -f installer.sh
+
+curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis/$BRANCH/installer.sh
+
+chmod +x alis.sh
+
+./installer.sh
