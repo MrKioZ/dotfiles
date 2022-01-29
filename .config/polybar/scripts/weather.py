d1cfe3640094c26388c05bdcff3f9aa2799e2ad0
Adam Sherif <38723412+MrKioZ@users.noreply.github.com>
2022-01-29 21:07:49 +0200

    Update weather.py

--- .config/polybar/scripts/weather.py
+++ .config/polybar/scripts/weather.py
@@ -13,7 +13,8 @@
 # LANG included thanks to krive001 on discord
 
 
-import requests
+import requests, json, os, time
+from datetime import datetime, timedelta
 
 CITY = ""
 API_KEY = ""
@@ -24,18 +25,44 @@ LANG = "en"
 #LANG = "nl"
 #LANG = "hu"
 
-# with open("cache/weather.json", "rw+") as f:
-#     data = json.loads(f.read())
+with open(f'/home/{os.getlogin()}/.config/polybar/scripts/cache/weather.json', 'r+') as f:
+    data = json.loads(f.read())
+    last_request = datetime.strptime(data['last_request'].split('.')[0], "%Y-%m-%dT%H:%M:%S")
 
 
-REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+if ((datetime.now() - last_request) >= timedelta(hours=12)) or (data['temp'] == 0):
+    try:
+        REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+        CURRENT = REQ["weather"][0]["main"].capitalize()
+        TEMP = int(float(REQ["main"]["temp"]))
 
-try:
-    # HTTP CODE = OK
-    CURRENT = REQ["weather"][0]["description"].capitalize()
-    TEMP = int(float(REQ["main"]["temp"]))
-    print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
-    # else:
-    #     print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
-except (ValueError, IOError):
-    print("Error: Unable print the data")
+        with open(f'/home/{os.getlogin()}/.config/polybar/scripts/cache/weather.json', 'w+') as f:
+            data['current'] = CURRENT
+            data['temp'] = TEMP
+            f.write(json.dumps(data))
+
+    except (ValueError, IOError):
+        print("Error: Unable print the data")
+else:
+    CURRENT = data['current']
+    TEMP = data['temp']
+
+# CURRENT = 'Rain'
+
+if CURRENT == "Thunderstorm":
+    CURRENT = " "
+elif CURRENT == "Drizzle":
+    CURRENT = ""
+elif CURRENT == "Rain":
+    CURRENT = ""
+elif CURRENT == "Snow":
+    CURRENT = ""
+elif CURRENT == "Clear":
+    CURRENT = ""
+elif CURRENT == "Clouds":
+    CURRENT = ""
+else:
+    CURRENT = ""
+
+print("{}  {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+
